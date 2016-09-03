d2b88d0c6445893eb96c64a016c2b31e40a87999
Silas Mahler <silas.mahler@fh-dortmund.de>
2016-09-03 18:06:11 +0200

    Angular2 läuft

--- app/app.component.js
+++ app/app.component.js
@@ -1,8 +1,8 @@
 (function(app) {
   app.AppComponent =
     ng.core.Component({
-      selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      selector: 'vesseltrip',
+      template: '<h1>Landing-Page VesselTrip</h1>'
     })
     .Class({
       constructor: function() {}
